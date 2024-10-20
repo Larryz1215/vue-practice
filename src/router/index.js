cc835cf07d9b6dc5906d8d6c89f6bf08d6de4b1e
Larry <tn708516@gmail.com>
2024-10-20 22:32:48 +0800

    test

---
+++ src/router/index.js
@@ -0,0 +1,40 @@
+import { createRouter, createWebHistory,createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHashHistory('/base-site/'),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView, 
+      children:[
+        {
+          path: 'base-site',
+          component: () => import('../views/AboutView.vue'), 
+          name: 'base-site',   
+          children:[
+            {
+              path:'book-detail',
+              component:() =>import('../components/BookDetail.vue'),
+              name: 'book-detail'
+            }
+           
+          ]
+        }     
+          
+      ]
+    },
+    
+  ]
+})
+router.beforeEach((to,from)=>{
+  // if(to.path == '/book-detail'){
+  //   console.log('book-detail')
+  //   return 'base-site/book-detail'
+  // }
+  console.log('to',to)
+  console.log('from',from)
+})
+
+export default router
